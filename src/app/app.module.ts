447e2276b705b4729f785b0c3afab64cec99ab45
Diana Garcia <diana00lgz@gmail.com>
2023-09-27 18:47:13 -0600

    27/09/2023 DLGZ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,21 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './prodct/product-list/product-list.component';
 import { ProductoFilterPipe } from './prodct/producto-filter.pipe';
 import { OperasBasComponent } from './formularios/operas-bas/operas-bas.component';
+import { CalculosComponent } from './formularios/calculos/calculos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductoFilterPipe,
-    OperasBasComponent //que pueda ser usado en todos los demas archivos
+    OperasBasComponent,
+    CalculosComponent //que pueda ser usado en todos los demas archivos
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
