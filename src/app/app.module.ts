4de84e952a5a8e2f66a06e24f61698378aff0023
Diana Garcia <diana00lgz@gmail.com>
2023-09-30 00:07:43 -0600

    11/09/2023 DLGZ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule } from '@angular/forms';
+import { MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './prodct/product-list/product-list.component';
@@ -9,6 +12,7 @@ import { ProductoFilterPipe } from './prodct/producto-filter.pipe';
 import { OperasBasComponent } from './formularios/operas-bas/operas-bas.component';
 import { CalculosComponent } from './formularios/calculos/calculos.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SumaComponent } from './formularios/suma/suma.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +20,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ProductListComponent,
     ProductoFilterPipe,
     OperasBasComponent,
-    CalculosComponent //que pueda ser usado en todos los demas archivos
+    CalculosComponent,
+    SumaComponent //que pueda ser usado en todos los demas archivos
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    MatInputModule,
+    MatButtonModule,
     BrowserAnimationsModule
   ],
   providers: [],
